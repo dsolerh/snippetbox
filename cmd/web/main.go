d50d4dcbe8a4aee4932d335704a608db02a549da
dsolerh <dsolerh.cinter95@gmail.com>
2022-03-20 11:15:22 -0400

    add config object to the app

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+// store configurations for the app
+type Config struct {
+	Addr      string
+	StaticDir string
+}
+
 func main() {
 	mux := http.NewServeMux()
+
+	// configuration
+	cfg := new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static", "Path to static asse")
+
+	flag.Parse()
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -14,10 +28,10 @@ func main() {
 	mux.HandleFunc("/file", downloadHandler)
 
 	// static files serve
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", cfg.Addr)
+	err := http.ListenAndServe(cfg.Addr, mux)
 	log.Fatal(err)
 }
