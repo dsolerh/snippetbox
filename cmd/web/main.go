99451329f2f3cc8c33509dfa071ebc9f2ec3a279
dsolerh <dsolerh.cinter95@gmail.com>
2022-03-20 00:14:34 -0400

    serve static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	// static files serve
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
