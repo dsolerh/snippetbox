d7e76dda177e71db9660fa68b6f3a27490d42517
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-27 09:29:24 -0400

    add flash message

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -35,7 +35,12 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, "show.page.tmpl", &templateData{Snippet: s})
+	flash := app.session.PopString(r, "flash")
+
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Flash:   flash,
+		Snippet: s,
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
@@ -64,6 +69,9 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
+
+	// store session data
+	app.session.Put(r, "flash", "Snippet created successfully!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
