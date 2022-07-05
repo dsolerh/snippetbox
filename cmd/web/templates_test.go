77efb35e503ff1cfd7f148f5849b0419edd2c864
dsolerh <dsolerh.cinter95@gmail.com>
2022-07-05 12:23:35 -0400

    update the test cases

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -6,12 +6,33 @@ import (
 )
 
 func TestHumanDate(t *testing.T) {
-	// Initialize a new time.Time object and pass it to humanDate function.
-	tm := time.Date(2022, 12, 17, 10, 0, 0, 0, time.UTC)
-	hd := humanDate(tm)
-	expected := "17 Dec 2022 at 10:00"
-
-	if hd != expected {
-		t.Errorf("want %q; got %q", expected, hd)
+	testCases := []struct {
+		desc string
+		tm   time.Time
+		want string
+	}{
+		{
+			desc: "UTC",
+			tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.UTC),
+			want: "17 Dec 2020 at 10:00",
+		},
+		{
+			desc: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			desc: "CET",
+			tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.FixedZone("CET", 1*6)),
+			want: "17 Dec 2020 at 09:00",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			hd := humanDate(tC.tm)
+			if hd != tC.want {
+				t.Errorf("want %q; got %q", tC.want, hd)
+			}
+		})
 	}
 }
