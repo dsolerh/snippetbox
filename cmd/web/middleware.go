2b2e52421395f236ae51a135419aa23cbc721d67
dsolerh <dsolerh.cinter95@gmail.com>
2022-07-05 09:45:28 -0400

    add nosurf middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
 func secureHeaders(next http.Handler) http.Handler {
@@ -11,6 +13,17 @@ func secureHeaders(next http.Handler) http.Handler {
 		w.Header().Set("X-Frame-Options", "deny")
 		next.ServeHTTP(w, r)
 	})
+}
+
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+
+	return csrfHandler
 }
 
 func (app *application) logRequest(next http.Handler) http.Handler {
@@ -38,8 +51,6 @@ func (app *application) requireAuthenticatedUser(next http.Handler) http.Handler
 			http.Redirect(w, r, "/user/login", http.StatusFound)
 			return
 		}
-
 		next.ServeHTTP(w, r)
-
 	})
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,7 +10,7 @@ import (
 func (app *application) routes() http.Handler {
 	// create a middleware chain
 	standardMiddleware := alice.New(app.panicRecover, app.logRequest, secureHeaders)
-	dynamicMiddleware := alice.New(app.session.Enable)
+	dynamicMiddleware := alice.New(app.session.Enable, noSurf)
 
 	mux := pat.New()
 
