f1d4e98f0bcd1315b0d6ec508a58d7ef327bec53
dsolerh <dsolerh.cinter95@gmail.com>
2022-07-05 21:21:36 -0400

    improve e2e testing

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,30 +1,37 @@
 package main
 
 import (
-	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
-func Test(t *testing.T) {
-	rr := httptest.NewRecorder()
+func TestPing(t *testing.T) {
 
-	r := httptest.NewRequest("GET", "/", nil)
+	// Create a new instance of our application struct. For now, this just
+	// contains a couple of mock loggers (which discard anything written to
+	// them).
+	app := newTestApplication(t)
 
-	ping(rr, r)
-	rs := rr.Result()
+	// We then use the httptest.NewTLSServer() function to create a new test
+	// server, passing in the value returned by our app.routes() method as the
+	// handler for the server. This starts up a HTTPS server which listens on a
+	// randomly-chosen port of your local machine for the duration of the test.
+	// Notice that we defer a call to ts.Close() to shutdown the server when
+	// the test finishes.
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	if rs.StatusCode != http.StatusOK {
-		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
+	// The network address that the test server is listening on is contained
+	// in the ts.URL field. We can use this along with the ts.Client().Get()
+	// method to make a GET /ping request against the test server. This
+	// returns a http.Response struct containing the response.
+	code, _, body := ts.get(t, "/ping")
+
+	if code != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, code)
 	}
 
-	defer rs.Body.Close()
-	body, err := ioutil.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
 	if string(body) != "Ok" {
-		t.Errorf("want body to equal %q", "OK")
+		t.Errorf("want body to equal %q", "Ok")
 	}
 }
